71520f124a2909e0d07c6d415657013502639fec
Pirate Jack <shchelkalinevgeniy@gmail.com>
2022-05-03 20:30:56 +0300

    styles added

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,8 @@
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <h1>ЖОПА</h1>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
